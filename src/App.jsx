5d7290ab4859be9f97cc56c452c86cc644a5139b
Saad-Alikhanov <104293685+Saad-Alikhanov@users.noreply.github.com>
2024-05-26 21:05:37 +0500

    layout modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import NavBar from './components/NavBar'
 import VideoPost from './components/VideoPost'
 import TextNews from './components/TextNews'
 import Products from './components/Products'
+import './css/layout.css'
 
 const App = () => {
   return (
     <>
       <NavBar></NavBar>
-      <VideoPost></VideoPost>
-      <TextNews></TextNews>
+      <div className='news-container'>
+        <VideoPost></VideoPost>
+        <TextNews></TextNews>
+      </div>
       <Products></Products>
     </>
   )
